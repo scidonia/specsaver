5b4a74aaa486c050e49754cccaa9e962a25a303a
Gavin Mendel-Gleason <gavin@scidonia.ai>
2026-07-23 09:17:16 +0200

    chore: remove build artifacts

--- docs/slidedev/dist-check/assets/slidev/default-CoBar0p2.js
+++
@@ -1,1 +0,0 @@
-import{o,e as t,i as s}from"../modules/vue-B36ANLLI.js";import{_ as r}from"../index-BJPSFeYe.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
